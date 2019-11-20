96a94e2bcdae6fc3e280767bcff0dce0dc8e7965
yukitohiroki <qfpng09604@yahoo.co.jp>
2019-11-20 23:29:22 +0900

    edit-view-file
    Viewの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
     belongs_to :user
+    validates :content,presence:true,length: { maximum: 200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do
     resources :comments, only: [:create]
+    collection do
+      get 'shows'
+    end
   end
   resources :users, only: [:show]
 end
